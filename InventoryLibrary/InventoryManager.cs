64dfecfd06ac54c924ac3f0b0b8da358a7c0dec7
Youssef Ahmed <youssefabojbara@gmail.com>
2026-02-02 05:01:04 +0200

    Initial

---
+++ InventoryLibrary/InventoryManager.cs
@@ -0,0 +1,48 @@
+﻿namespace InventoryLibrary;
+
+public static class InventoryManager
+{
+    static List<string> names = new();
+    static List<int> ids = new();
+    static List<int> quantities = new();
+    static List<int> prices = new();
+
+    public static void AddProduct()
+    {
+        Console.Write("Enter Name: ");
+        string? name = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Invalid Name!");
+            return;
+        }
+        names.Add(name);
+
+        Console.Write("Enter ID: ");
+        if (!int.TryParse(Console.ReadLine(), out int id))
+        {
+            Console.WriteLine("Invalid ID!");
+            return;
+        }
+        ids.Add(id);
+
+        Console.Write("Enter Quantity: ");
+        if (!int.TryParse(Console.ReadLine(), out int quantity))
+        {
+            Console.WriteLine("Invalid Quantity!");
+            return;
+        }
+        quantities.Add(quantity);
+
+        Console.Write("Enter Price: ");
+        if (!int.TryParse(Console.ReadLine(), out int price))
+        {
+            Console.WriteLine("Invalid Price!");
+            return;
+        }
+        prices.Add(price);
+
+        Console.WriteLine("✅ Product added successfully!");
+    }
+}
---
+++ InventoryManagement.UI/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using InventoryLibrary;
+
+namespace InventoryManagementSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true) 
+            {
+                ShowMenu();
+                string choice = Console.ReadLine() ?? "";
+
+                switch (choice)
+                {
+                    case "1":
+                        InventoryManager.AddProduct();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("--> Updating Product...");
+                        break;
+                    case "3":
+                        Console.WriteLine("--> Viewing Inventory...");
+                        break;
+                    case "4":
+                        Console.WriteLine("Exiting... Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+                Console.Clear(); 
+            }
+
+        }
+        static void ShowMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("========================================");
+            Console.WriteLine("   INVENTORY MANAGEMENT SYSTEM          ");
+            Console.WriteLine("========================================");
+            Console.ResetColor();
+            
+            Console.WriteLine("[1] Add New Product");
+            Console.WriteLine("[2] Update Product Quantity");
+            Console.WriteLine("[3] View Current Inventory");
+            Console.WriteLine("[4] Exit Close Program");
+            Console.Write("\nChoose an option (1-4): ");
+        }
+    }
+}
